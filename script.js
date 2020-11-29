5f4280082725d0e79fdc6fed20ef6d34c66ddc4b
fzramos <64625115+fzramos@users.noreply.github.com>
2020-11-28 18:41:21 -0600

    More HTML sturcutre added and inital js added -FR

---
+++ script.js
@@ -0,0 +1,7 @@
+const newItem = () => {
+    let task_text = document.getElementById('task_text').value;
+    console.log(task_text);
+
+    // copying creating, and appending new todo-task list item
+    // const list_item = document.createElement('')
+}
