8e75f4da2b2a7f732821d866671d46a59f815d63
fzramos <64625115+fzramos@users.noreply.github.com>
2020-11-29 00:18:03 -0600

    Form submit now adds Task to Tasks lists

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const newItem = () => {
     let task_text = document.getElementById('task_text').value;
-    console.log(task_text);
+    // console.log(task_text);
 
-    // copying creating, and appending new todo-task list item
+    // copying list item and appending new todo-task list item
     // const list_item = document.createElement('')
+    let todo_item = document.getElementById('todo-list').firstElementChild;
+    console.log(todo_item)
+    let new_todo_item = todo_item.cloneNode(true);
+
+    new_todo_item.firstElementChild.innerHTML = task_text;
+
+    document.getElementById("todo-list").appendChild(new_todo_item);
+}
+
+const completedTask = (elmnt) => {
+    console.log(elmnt.parentElement);
+    // TODO will move the contents of this element to the Completed list !without! button
+
 }
